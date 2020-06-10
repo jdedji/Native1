bcd7f3262dbe9ba6122a2402c0747079f1a220ff
jdedji <jeandedji2016@gmail.com>
2020-06-10 13:58:23 -0400

    one

---
+++ nucampsite/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Main from './Components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
+export default function App() {
+    return (
+        <Provider store={store}>
+        <Main />
+      </Provider>
+       
+    );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ nucampsite/Components/AboutComponent.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {FlatList, Text, ScrollView } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
+// import {PARTNERS} from '../shared/partners';
+
+const mapStateToProps = state => {
+    return {
+      partners: state.partners
+    };
+};
+function Mission(){
+    return(
+        <ScrollView>
+        <Card title= "Our Mission" style={{margin: 20}}wrapperStyle={{margin: 20}}>
+        <Text> 
+       We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+       </Text>
+      </Card> 
+    </ScrollView>
+    )
+}
+
+class About extends Component{
+// constructor(props){
+//    super(props)
+//    this.state= {
+//     partners :PARTNERS
+//    }
+
+// }
+
+    static navigationOptions = {
+        title: 'About Us'
+    }
+render(){
+    const renderPartner = ({item}) => {
+        return (
+            <ListItem
+                title={item.name}
+                subtitle={item.description}
+                leftAvatar={{source: {uri: baseUrl + item.image}}}
+                />
+        );
+    };
+
+    if (this.props.partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+
+    if (this.props.partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Text>{this.props.partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
+    return(
+        <ScrollView>
+            <Mission/>
+            <Card title="Community Partners">
+
+                <FlatList 
+                    data={this.props.partners.partners}
+                    renderItem={renderPartner}
+                    keyExtractor={item => item.id.toString()}
+                    />
+
+            </Card>
+            </ScrollView>
+    )
+}
+}
+export default connect(mapStateToProps)(About);
